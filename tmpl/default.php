4e6712371cfcff7c24a71698c4220eb26998bf4d
Piotr Moćko <mocko.piotr@gmail.com>
2014-07-08 20:58:50 +0000

    Poprawka okna modalnego Bootstrap 3

--- tmpl/default.php
+++ tmpl/default.php
@@ -49,7 +49,7 @@ $message =
     ?>
 	
     <!-- PRO START -->
-	<?php if ($layout == 'modal') : ?><div id="pwebcontact<?php echo $form_id; ?>_modal" class="pwebcontact-modal modal<?php if ((int)$params->get('bootstrap_version', 2) === 2) echo ' hide fade'; ?>" style="display:none"><?php endif; ?>
+	<?php if ($layout == 'modal') : ?><div id="pwebcontact<?php echo $form_id; ?>_modal" class="pwebcontact-modal modal fade<?php if ((int)$params->get('bootstrap_version', 2) === 2) echo ' hide'; ?>" style="display:none"><?php endif; ?>
 	<!-- PRO END -->
     
     <div id="pwebcontact<?php echo $form_id; ?>_box" class="pwebcontact-box <?php echo $params->get('moduleClass').' '.$params->get('boxClass'); ?>" dir="<?php echo $params->get('rtl', 0) ? 'rtl' : 'ltr'; ?>">
