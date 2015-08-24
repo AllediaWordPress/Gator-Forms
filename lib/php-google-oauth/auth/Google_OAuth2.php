102917fdb3bd4fc542f1139f920cbf90649a641a
mateuszpodraza <m.podraza@perfect-web.pl>
2015-08-24 18:57:04 +0200

    Removed a reference to non-existant class

--- lib/php-google-oauth/auth/Google_OAuth2.php
+++ lib/php-google-oauth/auth/Google_OAuth2.php
@@ -110,8 +110,7 @@ class Google_OAuth2 extends Google_Auth {
         if ($decodedResponse != null && $decodedResponse['error']) {
           $response = $decodedResponse['error'];
         }
-		ns_google_sheets_connector::ns_debug_log("Error fetching OAuth2 access token, message: '$response' :: {$request->getResponseHttpCode()}");
-		
+
         throw new Google_AuthException("Error fetching OAuth2 access token, message: '$response'", $request->getResponseHttpCode());
       }
     }
