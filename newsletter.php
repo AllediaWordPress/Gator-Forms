85214320729e094875265384d282434329c2addf
Andrzej Kawula <andrew@perfectdashboard.com>
2016-12-30 14:46:31 +0100

    fix for newsletter mailchimp.

--- newsletter.php
+++ newsletter.php
@@ -113,89 +113,116 @@ class PWebContact_Freshmail
 class PWebContact_Mailchimp
 {
 
-    public static function getLists($options)
-    {
-        $response = self::doRequest('lists/list.json', $options);
+	public static function getLists($options)
+	{
+		$response = self::doRequest('GET', 'lists', $options);
 
-        $lists = array();
-        if ($response && isset($response->data))
-        {
-            foreach ($response->data as $list)
-            {
-                $lists[$list->id] = $list->name;
-            }
-        }
+		$lists = array();
+		if ($response && isset($response->lists))
+		{
+			foreach ($response->lists as $list)
+			{
+				$lists[$list->id] = $list->name;
+			}
+		}
 
-        return $lists;
-    }
+		return $lists;
+	}
 
-    public static function subscribe($id_list, $email, $name = '', $fields = array(), $options = array())
-    {
-        $data = array(
-            'id' => $id_list,
-            'double_optin' => (!empty($options['opt']) ? true : false),
-            'email' => array(
-                'email' => $email
-            )
-        );
+	public static function subscribe($id_list, $email, $name = '', $fields = array(), $options = array())
+	{
+		$custom_fields = array();
+		foreach ($fields as $key => $value)
+		{
+			if (strpos($key, 'fm_') === 0)
+			{
+				$subkey = substr($key, 3);
+				if ($subkey !== 'email')
+				{
+					$custom_fields[$subkey] = $value;
+				}
+			}
+		}
 
-        if (!empty($name))
-        {
-            $fields['FNAME'] = $name;
-        }
-        $data['merge_vars'] = $fields;
+		if (!empty($name)) {
+			$custom_fields['FNAME'] = $name;
+		}
 
-        return self::doRequest('lists/subscribe.json', $options, $data, array('Content-Type: multipart/form-data'));
-    }
+		$data = array(
+			'email_address' => $email,
+			'email_type' => 'html',
+			'status' => (!empty($options['opt']) ? 'pending' : 'subscribed'),
+		);
 
-    protected static function doRequest($rest_path, $options, $data = array(), $headers = array())
-    {
-        if (!defined('PWEBCONTACT_DEBUG'))
-            define('PWEBCONTACT_DEBUG', false);
+		$result = self::doRequest('post', 'lists/' . $id_list . '/members', $options, json_encode($data),
+			array('Content-Type: multipart/form-data'));
 
-        $key_parts = explode('-', $options['apikey']);
-        if (empty($options['apikey']) || count($key_parts) < 2)
-        {
-            if (PWEBCONTACT_DEBUG)
-                PWebContact::setLog('Mailchimp error: ' . JText::_('MOD_PWEBCONTACT_BAD_API_KEY'));
-            return false;
-        }
+		// change fields
+		$_fields = array();
+		foreach($custom_fields as $key => $value) {
+			$_fields[strtoupper($key)] = $value;
+		}
+		unset($custom_fields);
 
-        $data['apikey'] = $options['apikey'];
-        $url            = 'https://' . $key_parts[1] . '.api.mailchimp.com/2.0/';
+		if($result && isset($result->id)) {
+			self::doRequest('patch', 'lists/' . $id_list . '/members/' . $result->id, $options, json_encode(array('merge_fields' => $_fields)),
+				array('Content-Type: multipart/form-data'));
+		}
+	}
 
-        try
-        {
-            $response = wp_remote_post($url . $rest_path, array(
-                'headers' => $headers,
-                'body' => $data,
-                'sslverify' => false
-            ));
+	protected static function doRequest($method = 'POST', $rest_path, $options, $data = array(), $headers = array())
+	{
+		if (!defined('PWEBCONTACT_DEBUG'))
+			define('PWEBCONTACT_DEBUG', false);
 
-            if (is_wp_error($response))
-            {
-                if (PWEBCONTACT_DEBUG)
-                    PWebContact::setLog('Mailchimp request error: ' . $response->get_error_message());
-                return false;
-            }
-            $result = json_decode($response['body']);
-        }
-        catch (Exception $e)
-        {
-            if (PWEBCONTACT_DEBUG)
-                PWebContact::setLog('Mailchimp HTTP request error: ' . $e->getMessage());
-            return false;
-        }
+		$key_parts = explode('-', $options['apikey']);
+		if (empty($options['apikey']) || count($key_parts) < 2)
+		{
+			if (PWEBCONTACT_DEBUG)
+				PWebContact::setLog('Mailchimp error: ' . JText::_('MOD_PWEBCONTACT_BAD_API_KEY'));
+			return false;
+		}
 
-        if (PWEBCONTACT_DEBUG)
-        {
-            if (isset($result->errors) && isset($result->errors[0]) && isset($result->errors[0]->error))
-                PWebContact::setLog('Mailchimp REST response: ' . $result->errors[0]->error);
-            elseif (isset($result->status) && $result->status == 'error')
-                PWebContact::setLog('Mailchimp REST response: ' . $result->error);
-        }
+		$url            = 'https://' . $key_parts[1] . '.api.mailchimp.com/3.0/';
+		$headers['Authorization'] = 'Basic ' . base64_encode($options['apikey'] . ':' . $options['apikey']);
 
-        return $result;
-    }
+		// filter method
+		$method = in_array(strtoupper($method), array('GET', 'PUT', 'POST', 'PATCH')) ? $method : 'POST';
+
+		try
+		{
+			$http = new WP_Http();
+			$response = $http->request($url . $rest_path, array(
+				'headers' => $headers,
+				'body' => $data,
+				'sslverify' => false,
+				'method' => strtoupper($method)
+			));
+
+			if (is_wp_error($response))
+			{
+				if (PWEBCONTACT_DEBUG)
+					PWebContact::setLog('Mailchimp request error: ' . $response->get_error_message());
+				return false;
+			}
+			$result = json_decode($response['body']);
+		}
+		catch (Exception $e)
+		{
+			if (PWEBCONTACT_DEBUG)
+				PWebContact::setLog('Mailchimp HTTP request error: ' . $e->getMessage());
+			return false;
+		}
+
+		if (PWEBCONTACT_DEBUG)
+		{
+			if (isset($result->errors) && isset($result->errors[0]) && isset($result->errors[0]->error))
+				PWebContact::setLog('Mailchimp REST response: ' . $result->errors[0]->error);
+			elseif (isset($result->status) && $result->status == 'error')
+				PWebContact::setLog('Mailchimp REST response: ' . $result->error);
+		}
+
+		return $result;
+	}
 
 }
