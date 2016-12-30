127074ae357634e1b26983bf801a8d24cbf5368f
Andrzej Kawula <andrew@perfectdashboard.com>
2016-12-30 14:46:14 +0100

    osx fix.

--- build/build.sh
+++ build/build.sh
@@ -3,18 +3,23 @@ echo "Building version: ${version}"
 
 php build.php
 cd free
-rm pwebcontact/media/js/jquery.pwebcontact.min.js
+rm -rf pwebcontact/media/js/jquery.pwebcontact.min.js
+mkdir -p pwebcontact/media/js
 unzip -j -o "wp_pwebcontact_${version}_free.zip" "pwebcontact/media/js/jquery.pwebcontact.min.js" -d "pwebcontact/media/js"
-yui-compressor "pwebcontact/media/js/jquery.pwebcontact.min.js" -o "pwebcontact/media/js/jquery.pwebcontact.min.js"
+yuicompressor "pwebcontact/media/js/jquery.pwebcontact.min.js" -o "pwebcontact/media/js/jquery.pwebcontact.min.js"
 zip -u "wp_pwebcontact_${version}_free.zip" "pwebcontact/media/js/jquery.pwebcontact.min.js"
-unzip -o -q "wp_pwebcontact_${version}_free.zip" -d "/home/piotr/www/wordpress-dev1/wp-content/plugins/"
-
+rm pwebcontact/media/js/jquery.pwebcontact.min.js
+rmdir -p pwebcontact/media/js
 cd ..
 
 php build.php --pro
 cd pro
-rm pwebcontact/media/js/jquery.pwebcontact.min.js
+mkdir -p pwebcontact/media/js
 unzip -j -o "wp_pwebcontact_${version}_pro.zip" "pwebcontact/media/js/jquery.pwebcontact.min.js" -d "pwebcontact/media/js"
-yui-compressor "pwebcontact/media/js/jquery.pwebcontact.min.js" -o "pwebcontact/media/js/jquery.pwebcontact.min.js"
+yuicompressor "pwebcontact/media/js/jquery.pwebcontact.min.js" -o "pwebcontact/media/js/jquery.pwebcontact.min.js"
 zip -u "wp_pwebcontact_${version}_pro.zip" "pwebcontact/media/js/jquery.pwebcontact.min.js"
-unzip -o -q "wp_pwebcontact_${version}_pro.zip" -d "/home/piotr/www/wordpress-dev2/wp-content/plugins/"
+unzip -o -q "wp_pwebcontact_${version}_pro.zip" -d "/Users/lanah/PhpstormProjects/wordpress/wp-content/plugins/"
+rm pwebcontact/media/js/jquery.pwebcontact.min.js
+rmdir -p pwebcontact/media/js
+
+echo "DONE"
