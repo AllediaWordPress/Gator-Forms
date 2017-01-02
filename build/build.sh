09da1245f99fb31d7f4323e9701d08f242d09403
Jan Krzywanek <j.krzywanek@perfect-web.pl>
2017-01-02 08:57:11 +0100

    Build script for me and Andrzej

--- build/build.sh
+++ build/build.sh
@@ -18,7 +18,13 @@ mkdir -p pwebcontact/media/js
 unzip -j -o "wp_pwebcontact_${version}_pro.zip" "pwebcontact/media/js/jquery.pwebcontact.min.js" -d "pwebcontact/media/js"
 yuicompressor "pwebcontact/media/js/jquery.pwebcontact.min.js" -o "pwebcontact/media/js/jquery.pwebcontact.min.js"
 zip -u "wp_pwebcontact_${version}_pro.zip" "pwebcontact/media/js/jquery.pwebcontact.min.js"
-unzip -o -q "wp_pwebcontact_${version}_pro.zip" -d "/Users/lanah/PhpstormProjects/wordpress/wp-content/plugins/"
+if [ -d "/Users/lanah/" ]; then
+  unzip -o -q "wp_pwebcontact_${version}_pro.zip" -d "/Users/lanah/PhpstormProjects/wordpress/wp-content/plugins/"
+  else
+  unzip -o -q "wp_pwebcontact_${version}_pro.zip" -d "/Users/DMR/Sites/wordpress/wp-content/plugins/"
+fi
+
+
 rm pwebcontact/media/js/jquery.pwebcontact.min.js
 rmdir -p pwebcontact/media/js
 
